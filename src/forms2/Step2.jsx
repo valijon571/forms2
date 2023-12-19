4be02431d58752834b85109d2d6c2343845282c8
valijon571 <qarshiboyevv85@gmail.com>
2023-12-19 14:54:29 +0500

    update

--- src/forms2/Step2.jsx
+++ src/forms2/Step2.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 const Step2 = () => {
   const [Step, setStep] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [post_loading, setPostLoading] = useState(false);
   const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
     name: "",
@@ -50,14 +51,14 @@ const Step2 = () => {
       })
       .catch((e) => {})
       .finally(() => {
-        // setLoading(false);
+        setLoading(false);
       });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    setLoading(true);
+    setPostLoading(true);
     let t = true,
       err = {};
     if (!formData.name) {
@@ -120,10 +121,10 @@ const Step2 = () => {
         })
         .catch((e) => {})
         .finally(() => {
-          setLoading(false);
+          setPostLoading(false);
         });
     } else {
-      setLoading(false);
+      setPostLoading(false);
       setErrors(err);
     }
   };
@@ -145,192 +146,197 @@ const Step2 = () => {
       <Step2Style>
         <div className="content">
           <form onSubmit={onSubmit}>
-            <div className="sc-klVQfs_bmxtkv">
-              <>
-                <div className="sc-jXbUNg_duTnwm">
-                  <b>Личные</b> данные
+            {loading ? (
+              <div>Yuklanmoqda.....</div>
+            ) : (
+              <div>
+                <div className="sc-klVQfs_bmxtkv">
+                  <div className="sc-jXbUNg_duTnwm">
+                    <b>Личные</b> данные
+                  </div>
+
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Контактный номер телефона</div>
+                    <div className="i_target">
+                      <div className="input_body_disabled">
+                        <InputMask
+                          placeholder=""
+                          name="name"
+                          mask="+998(nn) nnn-nn-nn"
+                          value={formData?.name}
+                          // onChange={handleChange}
+                          // placeholder="+998"
+                          formatChars={{
+                            n: "[0-9]",
+                            a: "[A-Za-z]",
+                          }}
+                        />
+                      </div>
+                    </div>
+                  </label>
+
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Фамилия</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          name="first_name"
+                          placeholder=""
+                          value={formData?.first_name}
+                          onChange={handleChange}
+                        />
+                        {errors.first_name && (
+                          <span className="error">{errors.first_name}</span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Имя</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          name="last_name"
+                          placeholder=""
+                          value={formData?.last_name}
+                          onChange={handleChange}
+                        />
+                        {errors.last_name && (
+                          <span className="error">{errors.last_name}</span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Отчество</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          type=""
+                          name="middle_name"
+                          placeholder=""
+                          value={formData?.middle_name}
+                          onChange={handleChange}
+                        />
+                        {errors.middle_name && (
+                          <span className="error">{errors.middle_name}</span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Дата рождения</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          type=""
+                          name="birthday"
+                          placeholder=""
+                          value={formData?.birthday}
+                          onChange={handleChange}
+                        />
+                        {errors.birthday && (
+                          <span className="error">{errors.birthday}</span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
                 </div>
 
-                <label className="sc-imWYAI_eGCxAi">
-                  <div class="label">Контактный номер телефона</div>
-                  <div className="i_target">
-                    <div className="input_body_disabled">
-                      <InputMask
-                        placeholder=""
-                        name="name"
-                        mask="+998(nn) nnn-nn-nn"
-                        value={formData?.name}
-                        // onChange={handleChange}
-                        // placeholder="+998"
-                        formatChars={{
-                          n: "[0-9]",
-                          a: "[A-Za-z]",
-                        }}
-                      />
-                    </div>
+                <div className="sc-klVQfs_bmxtkv">
+                  <div className="sc-jXbUNg_duTnwm">
+                    <b>Паспортные</b> данные
                   </div>
-                </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Тип документа</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <select
+                          placeholder="Passport"
+                          name="doc_type"
+                          value={formData?.doc_type}
+                          onChange={handleChange}
+                        >
+                          {errors.doc_type && (
+                            <span className="error">{errors.doc_type}</span>
+                          )}
+                          <option hidden="">Passport</option>
+                          <option value="1">Passport</option>
+                          <option value="2">Id Card</option>
+                        </select>
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Серия-Номер</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          mask="aa"
+                          placeholder="AA1234567"
+                          name="doc_number_serial"
+                          formatChars={{
+                            a: "[A-Za-z]",
+                            "*": "[A-Za-z0-9]",
+                          }}
+                          value={formData?.doc_number_serial}
+                          onChange={handleChange}
+                        />
+                        {errors.doc_number_serial && (
+                          <span className="error">
+                            {errors.doc_number_serial}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Дата выдачи</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          type="date"
+                          name="doc_given"
+                          placeholder=""
+                          value={formData?.doc_given}
+                          onChange={handleChange}
+                        />
+                        {errors.doc_given && (
+                          <span className="error">{errors.doc_given}</span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <label className="sc-imWYAI_eGCxAi">
+                    <div class="label">Серия и номер заграничного паспорта</div>
+                    <div className="i_target">
+                      <div className="input_body">
+                        <input
+                          type=""
+                          name="foreign_doc_number_serial"
+                          placeholder=""
+                          value={formData?.foreign_doc_number_serial}
+                          onChange={handleChange}
+                        />
+                        {errors.foreign_doc_number_serial && (
+                          <span className="error">
+                            {errors.foreign_doc_number_serial}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                </div>
 
-                <label className="sc-imWYAI_eGCxAi">
-                  <div class="label">Фамилия</div>
-                  <div className="i_target">
-                    <div className="input_body">
-                      <input
-                        name="first_name"
-                        placeholder=""
-                        value={formData?.first_name}
-                        onChange={handleChange}
-                      />
-                      {errors.first_name && (
-                        <span className="error">{errors.first_name}</span>
-                      )}
-                    </div>
-                  </div>
-                </label>
-                <label className="sc-imWYAI_eGCxAi">
-                  <div class="label">Имя</div>
-                  <div className="i_target">
-                    <div className="input_body">
-                      <input
-                        name="last_name"
-                        placeholder=""
-                        value={formData?.last_name}
-                        onChange={handleChange}
-                      />
-                      {errors.last_name && (
-                        <span className="error">{errors.last_name}</span>
-                      )}
-                    </div>
-                  </div>
-                </label>
-                <label className="sc-imWYAI_eGCxAi">
-                  <div class="label">Отчество</div>
-                  <div className="i_target">
-                    <div className="input_body">
-                      <input
-                        type=""
-                        name="middle_name"
-                        placeholder=""
-                        value={formData?.middle_name}
-                        onChange={handleChange}
-                      />
-                      {errors.middle_name && (
-                        <span className="error">{errors.middle_name}</span>
-                      )}
-                    </div>
-                  </div>
-                </label>
-                <label className="sc-imWYAI_eGCxAi">
-                  <div class="label">Дата рождения</div>
-                  <div className="i_target">
-                    <div className="input_body">
-                      <input
-                        type=""
-                        name="birthday"
-                        placeholder=""
-                        value={formData?.birthday}
-                        onChange={handleChange}
-                      />
-                      {errors.birthday && (
-                        <span className="error">{errors.birthday}</span>
-                      )}
-                    </div>
-                  </div>
-                </label>
-              </>
-            </div>
-
-            <div className="sc-klVQfs_bmxtkv">
-              <div className="sc-jXbUNg_duTnwm">
-                <b>Паспортные</b> данные
+                <div class="btns">
+                  {post_loading ? (
+                    <span class="sc-dhKdcB_htJRq"> "Yuklanmoqda"</span>
+                  ) : (
+                    <button type="submit" class="sc-dhKdcB_htJRq">Сохранить</button>
+                  )}
+                </div>
               </div>
-              <label className="sc-imWYAI_eGCxAi">
-                <div class="label">Тип документа</div>
-                <div className="i_target">
-                  <div className="input_body">
-                    <select
-                      placeholder="Passport"
-                      name="doc_type"
-                      value={formData?.doc_type}
-                      onChange={handleChange}
-                    >
-                      {errors.doc_type && (
-                        <span className="error">{errors.doc_type}</span>
-                      )}
-                      <option hidden="">Passport</option>
-                      <option value="1">Passport</option>
-                      <option value="2">Id Card</option>
-                    </select>
-                  </div>
-                </div>
-              </label>
-              <label className="sc-imWYAI_eGCxAi">
-                <div class="label">Серия-Номер</div>
-                <div className="i_target">
-                  <div className="input_body">
-                    <input
-                      mask="aa"
-                      placeholder="AA1234567"
-                      name="doc_number_serial"
-                      formatChars={{
-                        a: "[A-Za-z]",
-                        "*": "[A-Za-z0-9]",
-                      }}
-                      value={formData?.doc_number_serial}
-                      onChange={handleChange}
-                    />
-                    {errors.doc_number_serial && (
-                      <span className="error">{errors.doc_number_serial}</span>
-                    )}
-                  </div>
-                </div>
-              </label>
-              <label className="sc-imWYAI_eGCxAi">
-                <div class="label">Дата выдачи</div>
-                <div className="i_target">
-                  <div className="input_body">
-                    <input
-                      type="date"
-                      name="doc_given"
-                      placeholder=""
-                      value={formData?.doc_given}
-                      onChange={handleChange}
-                    />
-                    {errors.doc_given && (
-                      <span className="error">{errors.doc_given}</span>
-                    )}
-                  </div>
-                </div>
-              </label>
-              <label className="sc-imWYAI_eGCxAi">
-                <div class="label">Серия и номер заграничного паспорта</div>
-                <div className="i_target">
-                  <div className="input_body">
-                    <input
-                      type=""
-                      name="foreign_doc_number_serial"
-                      placeholder=""
-                      value={formData?.foreign_doc_number_serial}
-                      onChange={handleChange}
-                    />
-                    {errors.foreign_doc_number_serial && (
-                      <span className="error">
-                        {errors.foreign_doc_number_serial}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </label>
-            </div>
-            <div class="btns">
-              {/* {loading ? (
-                "Yuklanmoqda"
-              ) : ( */}
-              <button type="submit" class="sc-dhKdcB_htJRq">
-                {loading ? "Yuklanmoqda" : "Сохранить"}
-              </button>
-              {/* )} */}
-            </div>
+            )}
           </form>
         </div>
       </Step2Style>
